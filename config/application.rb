429cc83742a2df3596956aa0028611cf591d8338
Kristofers Kaprans <kristofers.kaprans@gmail.com>
2024-03-04 16:03:41 +0100

    Initial commit with devise template from https://github.com/lewagon/rails-templates

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,6 @@ module ChangeThisToYourRailsAppName
       generate.helper false
       generate.test_framework :test_unit, fixture: false
     end
-
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
 
