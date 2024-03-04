34e68aac07e3e3df25919a9ad18b3b75d1dae792
Kristofers Kaprans <kristofers.kaprans@gmail.com>
2024-03-04 17:30:07 +0100

    smtn

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,2 +1,112 @@
 class TripsController < ApplicationController
+  def index
+    @trips = Trip.all
+    render json: @trips
+  end
+
+  def show
+    # Retrieve a specific trip
+    @trip = Trip.find(params[:id])
+
+    # Get data from API
+    url = ENV['searchAI']
+    uri = URI(url)
+
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+
+    request = Net::HTTP::Post.new(uri.path)
+    request['Content-Type'] = 'application/json'
+
+    body = {
+      "instances": [
+        { "prompt": "what can i do in #{@trip.destination} from #{@trip.start_date} to #{@trip.end_date}?"}
+      ]
+    }
+
+    request.body = body.to_json
+
+    response = http.request(request)
+
+    if response.code == '200'
+      result = JSON.parse(response.body)
+      # Add the result to the trip data
+      @trip_data = @trip.attributes
+      @trip_data[:api_data] = result
+    else
+      # Handle the error here
+      @trip_data = @trip
+    end
+
+    render json: @trip_data
+  end
+
+  def show
+    # Retrieve a specific trip
+    @trip = Trip.find(params[:id])
+    render json: @trip
+  end
+
+  def create
+    # Create a new trip
+    require 'net/http'
+    @trip = Trip.new(trip_params)
+    if @trip.save
+      # Get data from API
+      url = ENV['searchAI']
+      uri = URI(url)
+
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = true
+
+      request = Net::HTTP::Post.new(uri.path)
+      request['Content-Type'] = 'application/json'
+
+      body = {
+        "instances": [
+          { "prompt": "what can i do in #{@trip.destination} from #{@trip.start_date} to #{@trip.end_date}?"}
+        ]
+      }
+
+      request.body = body.to_json
+
+      response = http.request(request)
+
+      if response.code == '200'
+        result = JSON.parse(response.body)
+        # Add the result to the trip data
+        @trip_data = @trip.attributes
+        @trip_data[:api_data] = result
+        render json: @trip_data, status: :created
+      else
+        # Handle the error here
+        render json: @trip.errors, status: :unprocessable_entity
+      end
+    else
+      render json: @trip.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    # Update an existing trip
+    @trip = Trip.find(params[:id])
+    if @trip.update(trip_params)
+      render json: @trip
+    else
+      render json: @trip.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    # Delete a trip
+    @trip = Trip.find(params[:id])
+    @trip.destroy
+    head :no_content
+  end
+
+  private
+
+  def trip_params
+    params.permit(:destination, :start_date, :end_date)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_04_153440) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_04_161905) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_04_153440) do
   create_table "trips", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "destination"
+    t.date "start_date"
+    t.date "end_date"
   end
 
   create_table "user_trips", force: :cascade do |t|
