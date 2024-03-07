d3b907070b8207771e70d15959bb9aa676cefce7
Kristofers Kaprans <kristofers.kaprans@gmail.com>
2024-03-07 15:24:49 +0100

    feat: prompting added

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,109 +1,160 @@
 class TripsController < ApplicationController
   def index
-    @trips = Trip.all
+    @trips = current_user.trips
   end
   def show
-    # Retrieve a specific trip
     @trip = Trip.find(params[:id])
-    render json: @trip
+    @trip.content = eval(@trip.content)
+    if @trip.additional_suggestions.nil? || @trip.additional_suggestions.empty?
+      additional_suggestions = get_additional_suggestions(@trip.destination, @trip.start_date, @trip.end_date, false)
+      @trip.update_attribute(:additional_suggestions, additional_suggestions[@trip.destination.strip]) if additional_suggestions[@trip.destination.strip]
+    end
   end
   def destroy_all
-    Trip.destroy_all
+    current_user.trips.destroy_all
     redirect_to root_path
   end
-
+  def pretty_content(content)
+    content.transform_values do |value|
+      value.gsub('**', '').gsub(/\n/, ' ')
+    end
+  end
   def create
-    @trip = Trip.new(trip_params)
-    if @trip.save
-      content = get_trip_suggestions(@trip.destination, @trip.start_date, @trip.end_date)
+    destinations = trip_params[:destination].split(',').map(&:strip)
+    @trips = []
+    errors = []
+
+    destinations.each do |destination|
+      @trip = current_user.trips.new(trip_params.except(:destination).merge(destination: destination))
+      content = get_trip_suggestions(@trip.destination, @trip.start_date, @trip.end_date, destinations.size > 1)
       if content.present?
-        @trip.update(content: content)
-        respond_to do |format|
-          format.html { redirect_to root_path, notice: 'Trip was successfully created.' }
-          format.json { render :show, status: :created, location: @trip }
-          format.js
+        pretty_content = pretty_content(content)
+        @trip.content = pretty_content
+        if @trip.save
+          @trips << @trip
+        else
+          errors << { destination: destination, error: @trip.errors.full_messages }
         end
       else
-        # Handle error from service or API call
-        render json: { error: "Failed to retrieve suggestions" }, status: :unprocessable_entity
+        errors << { destination: destination, error: "Failed to retrieve suggestions" }
+      end
+    end
+
+    if errors.empty?
+      respond_to do |format|
+        if destinations.size > 1
+          format.html { redirect_to trips_path, notice: 'Trip(s) were successfully created.' }
+          format.json { render json: @trips, status: :created }
+        else
+          format.html { redirect_to trip_path(@trips.first), notice: 'Trip was successfully created.' }
+          format.json { render json: @trips.first, status: :created }
+        end
+        format.js
       end
     else
-      render :new
+      render json: { errors: errors }, status: :unprocessable_entity
     end
   end
 
     def update
-      # Update an existing trip
-      @trip = Trip.find(params[:id])
+      @trip = current_user.trips.find(params[:id])
       if @trip.update(trip_params)
         render json: @trip
       else
         render json: @trip.errors, status: :unprocessable_entity
       end
     end
-
-    def destroy
+    def delete_specific
       @trip = Trip.find(params[:id])
       @trip.destroy
 
-      respond_to do |format|
-        format.html { redirect_to root_path, notice: 'Trip was successfully deleted.' }
-        format.json { head :no_content }
-        format.js
-      end
+      redirect_to trips_path, notice: 'Trip was successfully deleted.'
     end
-
     private
 
-    def get_trip_suggestions(destination, start_date, end_date)
-      # Load the service account key JSON file.
+    def get_additional_suggestions(destinations, start_date, end_date, limit_words)
+      destinations = [destinations] unless destinations.is_a?(Array)
+      results = {}
       service_account_key_file = File.open("/home/kaprans/code/Kaprans22/WiseVoyage/WiseVoyage/app/controllers/potent.json")
-      # Define the required scopes.
       scopes = ['https://www.googleapis.com/auth/cloud-platform']
-
-      # Create an authorization object from the service account key file and the scopes.
       authorizer = Google::Auth::ServiceAccountCredentials.make_creds(
         json_key_io: service_account_key_file,
         scope: scopes
       )
+      destinations.each do |destination|
+        authorizer.fetch_access_token!
+        access_token = authorizer.access_token
+        url = ENV['searchAI']
+        uri = URI(url)
 
-      # Request an access token.
-      authorizer.fetch_access_token!
+        http = Net::HTTP.new(uri.host, uri.port)
+        http.use_ssl = true
+        request = Net::HTTP::Post.new(uri.path)
+        request['Content-Type'] = 'application/json'
+        request['Authorization'] = "Bearer #{access_token}"
 
-      # Use the access token in the Authorization header.
-      access_token = authorizer.access_token
+        body = {
+          "instances": [
+            { "prompt": "Please suggest additional activities in #{destination} from #{start_date} to #{end_date}." + (limit_words ? " Word limit: 100" : "")}
+          ]
+        }
 
-      # Construct API request using the retrieved access token
-      url = ENV['searchAI']
-      uri = URI(url)
+        request.body = body.to_json
 
-      http = Net::HTTP.new(uri.host, uri.port)
-      http.use_ssl = true
+        response = http.request(request)
 
-      request = Net::HTTP::Post.new(uri.path)
-      request['Content-Type'] = 'application/json'
-      request['Authorization'] = "Bearer #{access_token}"
-
-      body = {
-        "instances": [
-          { "prompt": "what can i do in #{destination} from #{start_date} to #{end_date}?"}
-        ]
-      }
-
-      request.body = body.to_json
-
-      response = http.request(request)
-
-      if response.code == '200'
-        result = JSON.parse(response.body)
-        return result['predictions'][0]['content']
-      else
-        Rails.logger.error("API request failed with code #{response.code}")
-        return nil
+        if response.code == '200'
+          result = JSON.parse(response.body)
+          results[destination] = result['predictions'][0]['content']
+        else
+          Rails.logger.error("API request failed with code #{response.code} for destination #{destination}")
+        end
       end
+      results
+    end
+    def get_trip_suggestions(destinations, start_date, end_date, limit_words)
+      destinations = [destinations] unless destinations.is_a?(Array)
+      results = {}
+      service_account_key_file = File.open("/home/kaprans/code/Kaprans22/WiseVoyage/WiseVoyage/app/controllers/potent.json")
+      scopes = ['https://www.googleapis.com/auth/cloud-platform']
+      authorizer = Google::Auth::ServiceAccountCredentials.make_creds(
+        json_key_io: service_account_key_file,
+        scope: scopes
+      )
+      destinations.each do |destination|
+        authorizer.fetch_access_token!
+        access_token = authorizer.access_token
+        url = ENV['searchAI']
+        uri = URI(url)
+
+        http = Net::HTTP.new(uri.host, uri.port)
+        http.use_ssl = true
+        request = Net::HTTP::Post.new(uri.path)
+        request['Content-Type'] = 'application/json'
+        request['Authorization'] = "Bearer #{access_token}"
+
+        body = {
+          "instances": [
+            { "prompt": "Please describe me the country #{destination} for tourism" + (limit_words ? " Word limit: 50" : "")}
+          ]
+        }
+
+        request.body = body.to_json
+
+        response = http.request(request)
+
+        if response.code == '200'
+          result = JSON.parse(response.body)
+          results[destination] = result['predictions'][0]['content']
+        else
+          Rails.logger.error("API request failed with code #{response.code} for destination #{destination}")
+        end
+      end
+
+      results
     end
 
     def trip_params
-      params.permit(:destination, :start_date, :end_date, :content)
+      params.permit(:destination, :start_date, :end_date)
     end
 end
