b892400de2bd55af076f0d802602218372558e54
JanisJIvdris <janis.jekabs.ivdris@gmail.com>
2024-03-08 14:08:48 +0100

    loading bar test

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,8 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
-import "@popperjs/core"
-import "bootstrap"
-import "jquery/src/jquery"  // Add this line
-import "@rails/ujs"  // Add this line
+import "@hotwired/turbo-rails";
+import "controllers";
+import "@popperjs/core";
+import "bootstrap";
+//import "jquery/src/jquery"  // Add this line
+import "@rails/ujs"; // Add this line
+import "loading_bar";
