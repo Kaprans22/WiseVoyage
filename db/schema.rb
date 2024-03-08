bee23146a1e2e771a0b29e4aab05a9c7a675e9ed
Kristofers Kaprans <kristofers.kaprans@gmail.com>
2024-03-08 12:13:03 +0100

    added: content for trips

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_07_145503) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_08_111141) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -68,6 +68,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_07_145503) do
     t.date "end_date"
     t.bigint "user_id", null: false
     t.text "additional_suggestions"
+    t.string "content"
     t.index ["user_id"], name: "index_trips_on_user_id"
   end
 
