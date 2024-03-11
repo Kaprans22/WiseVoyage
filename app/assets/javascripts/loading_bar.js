2b5dc0d16d83203c5869e34db08478f05f33490c
JanisJIvdris <janis.jekabs.ivdris@gmail.com>
2024-03-11 12:50:32 +0100

    Loading bar added

--- app/assets/javascripts/loading_bar.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("turbolinks:load", function () {
-  var loader = document.querySelector(".svg-calLoader");
-  if (loader) {
-    loader.style.display = "block"; // Show the loading bar when the page starts loading
-    setTimeout(function () {
-      loader.style.display = "none"; // Hide the loading bar when the page finishes loading
-    }, 2000); // Adjust the timeout as needed
-  }
-});
